e988e3879165850a66d407e321c73c620674c2a5
dilane Uchiwa <sagueuwakam@gmail.com>
2024-10-16 15:44:53 +0100

    relatice path change to absolute

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp ./lib-systemd-system/firewall.service /lib/systemd/system/firewall.service
+cp /etc/init.d/security/lib-systemd-system/firewall.service /lib/systemd/system/firewall.service
 
 sudo systemctl enable firewall.service
 sudo systemctl start firewall.service
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-chmod u+x ./parefeu.sh
-./parefeu.sh
+chmod u+x /etc/init.d/security/parefeu.sh
+/etc/init.d/security/parefeu.sh
 
 apt update
 apt install fail2ban -y
-cp ./etc-fail2ban-jail.d/custum.conf /etc/fail2ban/jail.d/custum.conf
+cp /etc/init.d/security/etc-fail2ban-jail.d/custum.conf /etc/fail2ban/jail.d/custum.conf
 systemctl restart fail2ban
